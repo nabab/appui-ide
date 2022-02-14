eead72fc91aa80afa18693d777140546e488898d
nabab <thomas.nabet@gmail.com>
2022-02-14 14:25:48 +0100

    Fix on component

--- src/components/component/component.php
+++ src/components/component/component.php
@@ -2,7 +2,6 @@
 <div class="bbn-overlay">
   <div class="bbn-h-100 code">
     <bbn-router :autoload="false"
-                :scrollable="false"
                 :nav="true"
                 :source="routerSource"
                 ref="tabstripComponents"
