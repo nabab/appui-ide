d8c75dea6ad69b9e591742986e961941876547c4
nabab <thomas.nabet@gmail.com>
2023-02-03 22:32:47 +0100

    Fixed link on home

--- src/mvc/html/editor/home.php
+++ src/mvc/html/editor/home.php
@@ -6,7 +6,7 @@
   	<li><a :href="root + 'doc'" v-text="_('The bbn framework documentation')"></a></li>
   	<li><a :href="root + 'iconology'" v-text="_('The icons')"></a></li>
   	<li><a :href="core + 'special_chars'" v-text="_('The special characters')"></a></li>
-  	<li><a :href="core + 'constants'" v-text="_('The constants')"></a></li>
+  	<li><a :href="root + 'constants'" v-text="_('The constants')"></a></li>
   	<li><a :href="root + 'logs'" v-text="_('The log viewer')"></a></li>
   	<li><a :href="root + 'finder'" v-text="_('The finder')"></a></li>
   	<li><a :href="root + 'profiler'" v-text="_('The profiler')"></a></li>
