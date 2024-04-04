cefdd6ed4495f52e673364d05a7656e9e8e09cdb
nabab <thomas.nabet@gmail.com>
2024-04-04 19:32:52 +0200

    Re added PHP Info

---
+++ src/mvc/html/info/php.php
@@ -0,0 +1,9 @@
+<!-- HTML Document -->
+
+<div class="bbn-overlay">
+  <iframe class="bbn-100"
+             :src="url"
+             width="100%"
+             height="100%"
+             style="border: 0"/>
+</div>
