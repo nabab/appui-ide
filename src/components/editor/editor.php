f8cecf5bb43146b7286589a5aa792fa6d0635788
nabab <thomas.nabet@gmail.com>
2024-12-23 23:21:43 +0100

    Added a container in editor for scroll

--- src/components/editor/editor.php
+++ src/components/editor/editor.php
@@ -34,55 +34,57 @@
                   ref="mainMenu"/>
       </bbn-toolbar>
     </bbn-pane>
-    <bbn-pane>
-      <bbn-splitter orientation="horizontal"
-                    :resizable="true"
-                    :collapsible="true">
-        <bbn-pane :size="220"
-                  :min="40"
-                  :scrollable="false">
-          <div class="bbn-flex-height">
-            <div class="bbn-w-100 bbn-xspadding">
-              <bbn-input placeholder="Search Content"/>
+    <bbn-pane :scrollable="false">
+      <div class="bbn-overlay">
+        <bbn-splitter orientation="horizontal"
+                      :resizable="true"
+                      :collapsible="true">
+          <bbn-pane :size="220"
+                    :min="40"
+                    :scrollable="false">
+            <div class="bbn-flex-height">
+              <div class="bbn-w-100 bbn-xspadding">
+                <bbn-input placeholder="Search Content"/>
+              </div>
+              <div class="bbn-w-100 bbn-xspadding">
+                <bbn-dropdown v-if="currentPathType?.types"
+                              :source="currentPathType.types"
+                              source-value="type"
+                              source-url=""
+                              source-text="url"
+                              v-model="currentTypeCode"/>
+              </div>
+              <div class="bbn-flex-fill bbn-bottom-spadding">
+                <bbn-tree :source="root + 'data/tree'"
+                          bbn-if="treeData"
+                          :map="mapTree"
+                          :data="setNodeData"
+                          uid="uid"
+                          :storage="true"
+                          :storage-full-name="'appui-ide-type-th-' + source.project.id + '-' + currentRoot"
+                          ref="tree"
+                          @nodedoubleclick="treeNodeActivate"
+                          :icon-color="iconColor"
+                          :draggable="true"
+                          @move="moveNode"
+                          :menu="treeMenu"/>
+              </div>
             </div>
-            <div class="bbn-w-100 bbn-xspadding">
-              <bbn-dropdown v-if="currentPathType?.types"
-                            :source="currentPathType.types"
-                            source-value="type"
-                            source-url=""
-                            source-text="url"
-                            v-model="currentTypeCode"/>
-            </div>
-            <div class="bbn-flex-fill bbn-bottom-spadding">
-              <bbn-tree :source="root + 'data/tree'"
-                        bbn-if="treeData"
-                        :map="mapTree"
-                        :data="setNodeData"
-                        uid="uid"
+          </bbn-pane>
+          <bbn-pane>
+            <bbn-router :autoload="true"
+                        ref="router"
+                        :nav="true"
                         :storage="true"
-                        :storage-full-name="'appui-ide-type-th-' + source.project.id + '-' + currentRoot"
-                        ref="tree"
-                        @nodedoubleclick="treeNodeActivate"
-                        :icon-color="iconColor"
-                        :draggable="true"
-                        @move="moveNode"
-                        :menu="treeMenu"/>
-            </div>
-          </div>
-        </bbn-pane>
-        <bbn-pane>
-          <bbn-router :autoload="true"
-                      ref="router"
-                      :nav="true"
-                      :storage="true"
-                      :storage-full-name="'appui-ide-editor-router-' + source.project.id"/>
-        </bbn-pane>
-        <!--bbn-pane :size="200"
-                  :min="40"
-                  :collapsed="true">
-          Structure
-        </bbn-pane-->
-      </bbn-splitter>
+                        :storage-full-name="'appui-ide-editor-router-' + source.project.id"/>
+          </bbn-pane>
+          <!--bbn-pane :size="200"
+                    :min="40"
+                    :collapsed="true">
+            Structure
+          </bbn-pane-->
+        </bbn-splitter>
+      </div>
     </bbn-pane>
   </bbn-splitter>
   <bbn-loader v-else></bbn-loader>
