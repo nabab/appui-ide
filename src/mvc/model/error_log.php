5ebb2f0ae2ac42e2efe4e64691406106046d3852
nabab <thomas.nabet@gmail.com>
2025-08-28 12:17:34 +0200

    Fixed error_log json file location

--- src/mvc/model/error_log.php
+++ src/mvc/model/error_log.php
@@ -1,9 +1,9 @@
 <?php
 use bbn\X;
 use bbn\Str;
-/** @var $model \bbn\Mvc\Model*/
+/** @var \bbn\Mvc\Model $model */
 
-$file = $model->tmpPath().'logs/_php_error.json';
+$file = $model->dataPath().'logs/_php_error.json';
 if ( $model->inc->fs->isFile($file) ){
   $json = json_decode(file_get_contents($file), true);
   return ['data' => $json];
